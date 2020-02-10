90b96e8c992af0411e7345117e056b635d132f89
Abhimanyu Kumar <58354441+ak22011996@users.noreply.github.com>
2020-02-10 11:08:47 +0530

    up

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ export default class App extends React.Component {
 	constructor(props) {
 			super(props);
 			this.state = {
-				gitUsers : ['abhishek', 'abhimanyu', 'Ravi'], 
+				gitUsers : [], 
 				displayData:[],
-				textValue : ""
+				searchResult: [],
+				textValue:''
 			}
 	}
-	
+
 	saveRegistrationDetail = function() {
 		console.log(document.getElementById('nameInput').value);
 		console.log(document.getElementById('emailInput').value);
@@ -41,6 +42,7 @@ export default class App extends React.Component {
 	}
 	handlePageChange = function(){
 		document.getElementById('registerDiv').style.display='none';
+		//document.getElementById('dashboard').style.display='none';
 		document.getElementById('loginDiv').style.display='block';
 	}
 	handleLoginPage = function (){
@@ -51,11 +53,11 @@ export default class App extends React.Component {
 		  method: 'POST',
 		   headers: {
 			'Accept': 'application/json',
-			'Content-Type': 'application/json',*/
-			'Access-Control-Allow-Origin': '*',
+			'Content-Type': 'application/json',
+		/*	'Access-Control-Allow-Origin': '*',
 			'Access-Control-Allow-Methods': 'POST',
 			'Access-Control-Allow-Headers': 'Content-Type, Authorization'*/
-		 },
+		  },
 		  body: JSON.stringify({
 			username: document.getElementById('loginNameInput').value,
 			userpass: document.getElementById('loginPassInput').value
@@ -63,6 +65,7 @@ export default class App extends React.Component {
 		})
 		.then(res => res.json())
 		.then(res => {
+			//this.refreshPage();
 			if(res.response=='success'){
 				document.getElementById('registerDiv').style.display='none';
 				document.getElementById('loginDiv').style.display='none';
@@ -73,23 +76,66 @@ export default class App extends React.Component {
 		})
 	}
 	
+	getGitUsersInfo = function(userName) {
+		console.log('getGitUsersInfo');
+		var url = "https://api.github.com/search/users?q="+userName;
+		console.log(url);
+		
+		const request = async (e) => {
+			try {
+				const response = await fetch(url);
+				const json = await response.json();
+				console.log(json.items);
+				this.setState({
+					gitUsers: json.items
+				})
+			} catch(e){
+				console.log("error "+ e);
+			}
+			
+		}
+		request();
+		
+	}
 	handleChange = function(event){
 		var inputVal = event.target.value;
 		var filteredData=[];
 		if(inputVal){
-			filteredData = this.state.gitUsers.filter(function(gitUser){
-				if(gitUser.toLowerCase().match(inputVal.toLowerCase())){
-					return gitUser;
-				}
-			});
-			this.setState({
-				displayData : filteredData
-			});
-		} else {
-			this.setState({
-				displayData : filteredData
-			});
-		}
+			this.state.textValue=inputVal;
+			if(this.state.searchResult && this.state.searchResult.length >0){
+				console.log("INSIDE");
+				filteredData = this.state.searchResult.filter(function(gitUser){
+					if(gitUser.login.toLowerCase().match(inputVal.toLowerCase())){
+						return gitUser;
+					}
+				});
+			} 
+			if(filteredData && filteredData.length==0) {
+				this.getGitUsersInfo(inputVal);
+				if(this.state.gitUsers){
+					console.log("AKS");
+					filteredData = this.state.gitUsers.filter(function(gitUser){
+						if(gitUser.login.toLowerCase().match(inputVal.toLowerCase())){
+							return gitUser;
+						}
+					});
+					for(var i in filteredData){
+						if(!this.state.searchResult.includes(filteredData[i])){
+							this.state.searchResult.push(filteredData[i]);
+						}
+					}
+					console.log("AKS "+ filteredData);
+				} 
+			}
+		} 
+		this.setState({
+			displayData : filteredData
+		});
+		console.log("END");
+		console.log(this.state.searchResult);
+		console.log(this.state.displayData);
+		console.log(this.state.gitUsers);
+		console.log("END");
 	}
 	deleteUser = function(){
 		console.log("DeleteUser");
@@ -110,21 +156,25 @@ export default class App extends React.Component {
 					<h1>Register Yourself For Learning</h1>
 					Name : <input  type="text" id="nameInput" placeholder="Enter your Name "required />
 					Email : <input id="emailInput" type="email" placeholder="Enter your email"required />
-					Password : <input id="passInput" type="Password" placeholder="Enter your Password"required /><hr/>
+					Password : <input id="passInput" type="Password" placeholder="Enter your Password"required /><br/><hr/>
 					<button id="saveButton" onClick={()=>this.saveRegistrationDetail()}>Sign Up</button><br/>
 					<button onClick={()=>this.handlePageChange()}>Sign In</button>
 				</div>
 				<div id="loginDiv" style={{display: 'none'}}>
-					UserName : <input  type="text" id="loginNameInput" placeholder="Enter your email"required />
-					Password : <input type="Password" id="loginPassInput" placeholder="Enter your Password"required/ ><hr/>
+					UserName : <input  type="text" id="loginNameInput" placeholder="Enter your email"required /><br/>
+					Password : <input type="Password" id="loginPassInput" placeholder="Enter your Password"required/ ><br/>
 					<button id="loginButton" onClick={()=>this.handleLoginPage()}>Sign In</button>
 				</div>	
 				<div id="searchEngine" style={{display: 'none'}}>
 					<input type="text" onChange={this.handleChange.bind(this)} placeholder="Search here..." />
 					<div className="suggestions">
 						{this.state.displayData.map(function(v){
-						return (<div className='search'>{v} <button onClick = {()=>this.deleteUser()}>Delete</button></div>)
-					})}
+								return (<divclassName='search' style={{Color:'white'}}>{v.login} <button className="deleteButton" onClick={function(){ 
+									
+									alert('user deleted');
+								}}>Delete</button></div>)
+							})
+						}.
 					</div>
 				</div>
 			</div>	
